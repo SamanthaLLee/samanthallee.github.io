5c940cfd7f37599c76ed9b39c4ddd49946f19562
SamanthaLLee <samantha.lin.lee@gmail.com>
2022-01-20 22:05:53 -0500

    app-container class

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -5,7 +5,7 @@ import Footer from "./Footer"
 
 export default ({ children }) => (
 
-  <Container fluid className="theme-light">
+  <Container fluid className="px-0 theme-light app-container">
     <Header />
     <Container fluid className="pt-5 mt-5 min-vh-100 text-center">
       {children}
